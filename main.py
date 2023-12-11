e7d2f6e8bcdf0c27d53c7e378f88532fa7d96b45
Ashwini Bhide <60039504+slytherin007@users.noreply.github.com>
2023-12-11 22:14:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import random
+
+root=Tk()
+root.geometry("700x450")
+root.title("Roll Dice")
+
+label=Label(root, text="", font=("times", 260))
+
+def roll():
+    dice=['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+    label.configure(text=f'{random.choice(dice)} {random. choice(dice)}') 
+    label.pack()
+
+button=Button(root, text="lets roll....", width=40, height=5, font=18, bg="pink", bd=1, command=roll)
+button.pack(padx=10, pady=10)
+root.mainloop()
